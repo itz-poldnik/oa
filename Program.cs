5563da1c2560efb882bcf001dfb431e14f247d9b
itz-poldnik <115493003+itz-poldnik@users.noreply.github.com>
2023-09-29 02:09:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти площадь кольца, внутренний радиус которого равен r, а внешний - заданному числу R.
+Console.Write("Введите внутренний радиус =");
+double r1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите внешний радиус =");
+double r2 = Convert.ToInt32(Console.ReadLine());
+double s = Math.PI * (Math.Pow(r2, 2) - Math.Pow(r1, 2));
+Console.Write($"Площадь кольца ={s}");
